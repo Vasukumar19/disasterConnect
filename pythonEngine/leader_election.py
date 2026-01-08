47e876670dda1b0f8f80dd4fe35bd0bdaab6ced2
Vasukumar19 <thadakalavasu@gmail.com>
2026-01-08 19:27:17 +0530

    Delete pythonEngine directory

--- pythonEngine/leader_election.py
+++
@@ -1,8 +0,0 @@
-import socket
-from discovery import peers
-
-def my_ip():
-    return socket.gethostbyname(socket.gethostname())
-
-def elect_leader():
-    return max(list(peers.keys()) + [my_ip()])
